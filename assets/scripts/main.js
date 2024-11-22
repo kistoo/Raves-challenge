0f144f22a5f17d7125d9b3d17293b5d96a27fc32
kistoo <tomio.nakamura15@gmail.com>
2024-11-21 21:45:13 -0500

    Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,39 @@
-const carouselInner = document.querySelector('.carousel-inner');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const totalItems = carouselItems.length;
-let currentIndex = 0;
+const carouselInnerWeb = document.querySelector('.carousel-web');
+const carouselWeb = carouselInnerWeb.querySelectorAll('.carousel-item');
+const totalItemsWeb = carouselWeb.length;
+let currentIndexWeb = 0;
 
-document.querySelector('.carousel-control.left').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-    updateCarousel();
+document.querySelector('.carousel-control.left.web').addEventListener('click', () => {
+    currentIndexWeb = (currentIndexWeb - 1 + totalItemsWeb) % totalItemsWeb;
+    updateCarouselWeb();
 });
 
-document.querySelector('.carousel-control.right').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % totalItems;
-    updateCarousel();
+document.querySelector('.carousel-control.right.web').addEventListener('click', () => {
+    currentIndexWeb = (currentIndexWeb + 1) % totalItemsWeb;
+    updateCarouselWeb();
 });
 
-function updateCarousel() {
-    const offset = -currentIndex * 100;
-    carouselInner.style.transform = `translateX(${offset}%)`;
+function updateCarouselWeb() {
+    const offset = -currentIndexWeb * 100;
+    carouselInnerWeb.style.transform = `translateX(${offset}%)`;
+}
+
+const carouselInnerMobile = document.querySelector('.carousel-mobile');
+const carouselMobile = carouselInnerMobile.querySelectorAll('.carousel-item');
+const totalItemsMobile = carouselMobile.length;
+let currentIndexMobile = 0;
+
+document.querySelector('.carousel-control.left.mobile').addEventListener('click', () => {
+    currentIndexMobile = (currentIndexMobile - 1 + totalItemsMobile) % totalItemsMobile;
+    updateCarouselMobile();
+});
+
+document.querySelector('.carousel-control.right.mobile').addEventListener('click', () => {
+    currentIndexMobile = (currentIndexMobile + 1) % totalItemsMobile;
+    updateCarouselMobile();
+});
+
+function updateCarouselMobile() {
+    const offset = -currentIndexMobile * 100;
+    carouselInnerMobile.style.transform = `translateX(${offset}%)`;
 }
